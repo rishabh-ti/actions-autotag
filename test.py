a50e61a7c2f4cd2e7172afbc62612244e6c7c1aa
Rishabh Raizada <110235735+rishabh-ti@users.noreply.github.com>
2023-03-06 22:10:21 +0530

    Update test.py

--- test.py
+++ test.py
@@ -11,10 +11,3 @@ print("\nArguments passed:", end = " ")
 for i in range(1, n):
 	print(sys.argv[i], end = " ")
 	
-# Addition of numbers
-Sum = 0
-# Using argparse module
-for i in range(1, n):
-	Sum += int(sys.argv[i])
-	
-print("\n\nResult:", Sum)
